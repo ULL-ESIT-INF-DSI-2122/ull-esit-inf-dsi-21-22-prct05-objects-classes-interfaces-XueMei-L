f21e1be0714f66774d10909e331f3d305c10a735
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-15 22:37:25 +0000

    constructor implementado, y tests comprobados

--- src/ejercicio-1/pokedex.ts
+++ src/ejercicio-1/pokedex.ts
@@ -15,5 +15,29 @@ export class Pokedex {
     private type:string;
     private info = Array(4);
 
+    /**
+     * _Constructor de la clase Pokedex_
+     * @param name nombre de pokemon
+     * @param weight peso de pokemon
+     * @param height altura de pokemon
+     * @param type tipo de pokemon
+     * @param info informacion que contiene ataque, defensa, velocidad, HP de pokemon
+     */
+    constructor(name:string, weight:number, height:number, 
+        type:string, info:number[]) {
+            this.name = name;
+            this.weight = weight;
+            this.height = height;
+            this.type = type;
+            this.info = info;
+        }
 
+    getName() { return this.name; }
+    getWeight() { return this.weight; }
+    getHeight() { return this.height; }
+    getType() { return this.type; }
+    getAttack() { return this.info[0]; }
+    getDefense() { return this.info[1]; }
+    getSpeed() { return this.info[2]; }
+    getHP() { return this.info[3]; }
 }
--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -5,26 +5,96 @@ import { Pokedex } from "../src/ejercicio-1/pokedex";
 
 
 describe('Tests for POKEMON GAME', ()=>{
-  describe('Tests for the class Combat', () => {
+
+    const pikachu:Pokedex = new Pokedex("pikachu", 50, 45, "electric", [90, 55, 110, 60]);
+    const charizard:Pokedex = new Pokedex("charizard", 60, 55, "fire", [84, 78, 100, 78]);
+
+    describe('Tests for the class Combat', () => {
     
-    let pikachu = new Pokedex("pikachu", 50, 45, "electric", [90, 55, 110, 60]);
-    let charizard = new Pokedex("charizard", 60, 55, "fire", [84, 78, 100, 78]);
-    let blastoise = new Pokedex("blastoise", 80, 50, "water", [83, 100, 78, 79]);
-
-    it('Existe una clase combat', () => {
-        expect(Combat).not.to.be.null;
-    });
-  });
-
-  describe('Tests for the class Pokedex', () => {
-    it('Existe una clase Pokedex', () => {
-        expect(Pokedex).not.to.be.null;
+        it('Existe una clase combat', () => {
+            expect(Combat).not.to.be.null;
+        });
     });
 
-    it('Existe objetos instanciados de la clase Pokedex', () => {
-        expect(pikachu).not.to.be.null;
-        expect(charizard).not.to.be.null;
-        expect(blastoise).not.to.be.null;
-    })
-  });
+    describe('Tests for the class Pokedex', () => {
+        it('Existe una clase Pokedex', () => {
+            expect(Pokedex).not.to.be.null;
+        });
+
+        it('Existe objetos instanciados de la clase Pokedex', () => {
+            expect(pikachu).not.to.be.null;
+            expect(charizard).not.to.be.null;
+        })
+
+        describe('Tests para el objeto pikachu (class Pokedex)', () => {
+            
+            it('Nombre = pikachu', () => {
+                expect(pikachu.getName()).to.be.equal("pikachu");
+            });
+
+            it('peso = 50', () => {
+                expect(pikachu.getWeight()).to.be.eq(50);
+            });
+
+            it('altura = 45', () => {
+                expect(pikachu.getHeight()).to.be.eq(45);
+            });
+
+            it('tipo = electric', () => {
+                expect(pikachu.getType()).to.be.equal("electric");
+            });
+
+            it('ataque = 90', () => {
+                expect(pikachu.getAttack()).to.be.eq(90);
+            });
+
+            it('defensa = 55', () => {
+                expect(pikachu.getDefense()).to.be.eq(55);
+            });
+
+            it('velocidad = 110', () => {
+                expect(pikachu.getSpeed()).to.be.eq(110);
+            });
+
+            it('HP = 60', () => {
+                expect(pikachu.getHP()).to.be.eq(60);
+            });
+        });
+
+        describe('Tests para el objeto charizard (class Pokedex)', () => {
+            
+            it('Nombre = charizard', () => {
+                expect(charizard.getName()).to.be.equal("charizard");
+            });
+
+            it('peso = 60', () => {
+                expect(charizard.getWeight()).to.be.eq(60);
+            });
+
+            it('altura = 55', () => {
+                expect(charizard.getHeight()).to.be.eq(55);
+            });
+
+            it('tipo = fire', () => {
+                expect(charizard.getType()).to.be.equal("fire");
+            });
+
+            it('ataque = 84', () => {
+                expect(charizard.getAttack()).to.be.eq(84);
+            });
+
+            it('defensa = 78', () => {
+                expect(charizard.getDefense()).to.be.eq(78);
+            });
+
+            it('velocidad = 100', () => {
+                expect(charizard.getSpeed()).to.be.eq(100);
+            });
+
+            it('HP = 78', () => {
+                expect(charizard.getHP()).to.be.eq(78);
+            });
+        });
+        
+    });
 });
