39c805ca4189451d098bf7afd7c35e6e1de02941
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-15 21:22:56 +0000

    test para comprobar las clases combat y pokedex, tests fallan

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Practica 04: Arrays, tuplas y enumerados
+# Practica 05: objects, classes, interfaces 
 
 ```
 >>  COMPONENTES: XueMei Lin
@@ -6,7 +6,7 @@
 >>  UNIVERSIDAD: Universidad de La Laguna
 >>  ASIGNATURA:  Desarrollo de sistemas informaticos
 >>  VERSION:     0.0.4
->>  FECHA:       13-03-2022
+>>  FECHA:       20-03-2022
 >>  GITHUB:      https://github.com/XueMei-L
 ```
 
--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -1,12 +1,20 @@
-// import 'mocha';
-// import { expect } from 'chai';
-// import { productTable } from "../src/ejercicio-1";
+import 'mocha';
+import { expect } from 'chai';
+import { Combat } from "../src/ejercicio-1/combat";
+import { Pokemon } from "../src/ejercicio-1/pokedex";
 
-// describe(' Tests for exercise 01', ()=>{
-//   describe('productTable function test', () => {
+
+describe('Tests for POKEMON GAME', ()=>{
+//   describe('Tests for the class Combat', () => {
     
-//     it(, () => {
+//     it('Existe una clase combat', () => {
 //       expect();
 //     });
 //   });
-// });
+
+  describe('Tests for the class Pokedex', () => {
+    it('Existe una clase Pokedex', () => {
+        expect(Pokemon).not.to.be.null;
+    });
+  });
+});
