21fb2dcae6ad62f5c2bb5b1824d8722f77b7441a
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-16 18:16:24 +0000

    test para comprobar que existe objeto instanciado de la clase Grid, test falla

--- tests/ejercicio-2/ejericico-2.spec.ts
+++ tests/ejercicio-2/ejericico-2.spec.ts
@@ -1,3 +1,16 @@
-// import 'mocha';
-// import { expect } from 'chai';
-// import {  } from "../src/ejercicio-2";
+import 'mocha';
+import { expect } from 'chai';
+import { Grid } from "../src/ejercicio-2/grid";
+
+describe('Tests for ejercicio-2 Conect 4', () =>{
+
+    const grid = new Grid();
+
+    describe('Tests for the class Grid', () => {
+
+        it('Existe una clase Pokedex', () => {
+            expect(grid).not.to.be.null;
+        });
+
+    });
+});
