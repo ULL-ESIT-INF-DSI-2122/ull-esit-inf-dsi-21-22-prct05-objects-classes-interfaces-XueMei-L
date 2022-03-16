d6045f2ae6fc663167526f7981e5f7266fb83497
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-16 18:26:03 +0000

    test para el metodo showGrid() en la clase Grid, test falla

--- tests/ejercicio-2/ejericico-2.spec.ts
+++ tests/ejercicio-2/ejericico-2.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { expect } from 'chai';
-import { Grid } from "../src/ejercicio-2/grid";
+import { Grid } from "../../src/ejercicio-2/grid";
 
 describe('Tests for ejercicio-2 Conect 4', () =>{
 
@@ -8,9 +8,12 @@ describe('Tests for ejercicio-2 Conect 4', () =>{
 
     describe('Tests for the class Grid', () => {
 
-        it('Existe una clase Pokedex', () => {
+        it('Existe una clase Grid', () => {
             expect(grid).not.to.be.null;
         });
 
+        it('Existe un metodo en la clase Grid llamado showGrid()', () =>{
+            expect(grid.showGrid()).not.to.be.null;
+        });
     });
 });
