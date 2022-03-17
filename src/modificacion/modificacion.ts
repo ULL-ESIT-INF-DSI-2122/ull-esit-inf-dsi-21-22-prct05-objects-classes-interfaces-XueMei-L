d8caa6a4356a6251025db42906d2ff26c644a00e
XueMei_L <alu0101225845@ull.edu.es>
2022-03-17 09:56:54 +0000

    implemetantado .toString() y .valueOf(), test corregidos

--- src/modificacion/modificacion.ts
+++ src/modificacion/modificacion.ts
@@ -1,3 +1,6 @@
+/**
+ * Class Hexadecimal to represents a value in hexadecimal.
+ */
 export class Hexadecimal {
     private numeroEntero:number = 0;
 
@@ -5,9 +8,36 @@ export class Hexadecimal {
         this.numeroEntero = numEntero;
     }
 
+    /**
+     * _Method that return own number_
+     * @returns own number.
+     */
+    public valueOf() { return this.numeroEntero; }
+
+    
+    /**
+     * _Method that return a number to hexadecimal in string_
+     */
+    public toString():string {
+        let hex = this.numeroEntero.toString(16);
+        if (hex.length < 2) {
+            hex = "0" + hex;
+        }else{
+            hex = "0x" + hex;
+        }
+        return hex;
+    }
 }
 
 
 const myHexNumber = new Hexadecimal(38)
 myHexNumber.toString() // returns the string "0x26"
+let a = myHexNumber.toString();
+console.log(`${a}`);
 myHexNumber.valueOf() // returns the number 38
+
+
+let myFirstHexValue = new Hexadecimal(23)
+let mySecondHexValue = new Hexadecimal(15)
+myFirstHexValue.add(mySecondHexValue).valueOf() // returns the number 38
+myFirstHexValue.add(mySecondHexValue).toString() // returns the string "0x26"
--- tests/modificacion/test.spec.ts
+++ tests/modificacion/test.spec.ts
@@ -5,7 +5,7 @@ import { Hexadecimal } from "../../src/modificacion/modificacion";
 
 describe('Tests for the class Hexadecimal', ()=>{
     
-    let  objHexadecimal = new Hexadecimal(30);
+    let  objHexadecimal = new Hexadecimal(38);
 
     describe('Tests for the class Hexadecimal', () => {
 
@@ -19,6 +19,18 @@ describe('Tests for the class Hexadecimal', ()=>{
 
         it('Existe un metodo llamado .toString()', () => {
             expect(objHexadecimal.valueOf()).not.to.be.null;
+        });
+
+        it('Existe un metodo llamado .add()', () => {
+            expect(objHexadecimal.add()).not.to.be.null;
+        });
+
+        it('Existe un metodo llamado .sub()', () => {
+            expect(objHexadecimal.sub()).not.to.be.null;
+        });
+
+        it('objHexadecimal.toString() retorna 0x26', () => {
+            expect(objHexadecimal.toString()).to.eq("0x26");
         });
     });
         
