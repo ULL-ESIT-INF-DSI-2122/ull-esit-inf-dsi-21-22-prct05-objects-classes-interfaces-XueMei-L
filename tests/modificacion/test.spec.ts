81a81968d328b118c7da1b436a904ca44ef45cbb
XueMei_L <alu0101225845@ull.edu.es>
2022-03-17 09:58:41 +0000

    tests para metodo add() y sub(), tests fallan

--- tests/modificacion/test.spec.ts
+++ tests/modificacion/test.spec.ts
@@ -5,7 +5,12 @@ import { Hexadecimal } from "../../src/modificacion/modificacion";
 
 describe('Tests for the class Hexadecimal', ()=>{
     
-    let  objHexadecimal = new Hexadecimal(38);
+    // Objetos para la clase Hexadecimal para realizar transformacion del numero
+    let objHexadecimal = new Hexadecimal(38);
+
+    // Objetos para la clase Hexadecimal para realizar la suma y la resta
+    let myFirstHexValue = new Hexadecimal(23)
+    let mySecondHexValue = new Hexadecimal(15)
 
     describe('Tests for the class Hexadecimal', () => {
 
@@ -28,6 +33,7 @@ describe('Tests for the class Hexadecimal', ()=>{
         it('Existe un metodo llamado .sub()', () => {
             expect(objHexadecimal.sub()).not.to.be.null;
         });
+
 
         it('objHexadecimal.toString() retorna 0x26', () => {
             expect(objHexadecimal.toString()).to.eq("0x26");
