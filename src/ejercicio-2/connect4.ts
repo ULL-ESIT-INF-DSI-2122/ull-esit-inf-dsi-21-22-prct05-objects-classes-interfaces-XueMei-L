a0a4cdc1d4b69484820e8f6607580e11ef9750a3
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-20 18:19:26 +0000

    creado el metodo gameWinner() (class Conect4), test corregido

--- src/ejercicio-2/connect4.ts
+++ src/ejercicio-2/connect4.ts
@@ -72,7 +72,103 @@ export class GameConect4 {
 
 
 
-    
+    public gameWinner(posActualI:number, posActualJ:number):boolean {
+        let winner:boolean = false;
+        let winnerToken:number = 0;
+        let countToken:number;
+
+        //hay que hacer 4 bucle for para comprobar
+
+        //horizontal
+        countToken = 0;
+        // console.log(`${grid.getCols()}`);
+        for(let i:number = 0; i < grid.getCols()-1; i++) {
+            if(grid.getGrid()[posActualI][i] == 0) {
+                continue;
+            }else if(grid.getGrid()[posActualI][posActualJ] == grid.getGrid()[posActualI][i]) {
+                if(grid.getGrid()[posActualI][i] == grid.getGrid()[posActualI][i+1]){
+                    countToken++;
+                    if(countToken==3) {
+                        winner = true;
+                        winnerToken = grid.getGrid()[posActualI][posActualJ];
+                        // console.log(`es: ${grid.getGrid()[posActualI][posActualJ]}`);
+                    }
+                }
+            }else{
+                continue;
+            }
+        }
+
+        //vertical
+        countToken = 0;
+        // console.log(`${grid.getRows()}`);
+        for(let i:number = posActualI; i < grid.getRows()-1; i++) {
+            if(grid.getGrid()[i][posActualJ] == 0) {
+                continue;
+            }else if(grid.getGrid()[posActualI][posActualJ] == grid.getGrid()[i][posActualJ]) {
+                if(grid.getGrid()[i][posActualJ] == grid.getGrid()[i+1][posActualJ]){
+                    countToken++;
+                    if(countToken==3) {
+                        winner = true;
+                        // console.log(`${winner}`);
+                    }
+                    // console.log(`${countToken}`);
+                }
+            }else{
+                continue;
+            }
+        }
+
+
+        //diagonal izq
+        countToken = 0;
+        let initCountPosI_izq:number = posActualI + posActualJ;
+        let initCountPosJ_izq:number = 0;
+        if(initCountPosI_izq < grid.getRows()) {
+            initCountPosI_izq = 0;
+        }else{
+            initCountPosI_izq = initCountPosI_izq - grid.getRows();
+            initCountPosJ_izq = grid.getCols()-1;
+        }
+        for(let i:number = initCountPosI_izq, j:number = initCountPosJ_izq; i < grid.getRows()-1; i++, j--) {
+            // console.log(`wo zai i = ${i} j = ${j}`);
+
+            if(grid.getGrid()[i][j] == 0) {
+                continue;
+            }else if(grid.getGrid()[i][j] == grid.getGrid()[i+1][j-1]) {
+                countToken++;
+                    if(countToken==3) {
+                        winner = true;
+                        // console.log(`${winner}`);
+                    }
+            }else{
+                continue;
+            }
+        }
+
+        //diagonal der
+        countToken = 0;
+        let initCountPosI_der:number = posActualI - Math.min(posActualI, posActualJ);
+        let initCountPosJ_der:number = posActualJ - Math.min(posActualI, posActualJ);
+        // console.log(`${initCountPosI_der}, ${initCountPosJ_der}`)
+
+        for(let i:number = initCountPosI_der, j:number = initCountPosJ_der; i < grid.getRows()-1; i++, j++) {
+            // console.log(`wo zai i = ${i} j = ${j}`);
+
+            if(grid.getGrid()[i][j] == 0) {
+                continue;
+            }else if(grid.getGrid()[i][j] == grid.getGrid()[i+1][j+1]) {
+                countToken++;
+                    if(countToken==3) {
+                        winner = true;
+                        // console.log(`${winner}`);
+                    }
+            }else{
+                continue;
+            }
+        }
+        return winner;
+    }
 }
                 
 let a:Player = new Player("Juan");
